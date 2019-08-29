bf773b294774de32b07366d057af065ee2a52bb4
Kelly Lin <xiaoyumumu911@163.com>
2019-08-29 09:41:23 +0800

    First Commit

---
+++ src/main/java/com/kellylin1115/interviewquestionsblog/InterviewQuestionsBlogApplication.java
@@ -0,0 +1,13 @@
+package com.kellylin1115.interviewquestionsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionsBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQuestionsBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kellylin1115/interviewquestionsblog/InterviewQuestionsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellylin1115.interviewquestionsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
